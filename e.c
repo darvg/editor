3e1cd2e60e5148f102c59dc39790d682d3baa992
Siddharth Gollapudi <siddharthgollapudi99@gmail.com>
2021-07-30 21:52:58 -0700

    Add in incremental vi-style file search

--- e.c
+++ e.c
@@ -83,6 +83,7 @@ int getWindowSize(int *rows, int *cols);
 // input headers
 void edProcessStroke();
 void edMoveCursor(int c);
+char *edPrompt(char *prompt, void (*callback)(char *, int));
 
 // output headers
 void edClearScreen();
@@ -104,6 +105,7 @@ void edUpdateRow(edRow *row); //help us handle tabs
 void edDeleteRow(int at);
 void edFreeRow(edRow *row);
 int edComputeRx(edRow *row, int cX);
+int edComputeCx(edRow *row, int rX);
 void edRowInsertChar(edRow *row, int at, int c);
 void edRowRemoveChar(edRow *row, int at);
 void edRowAppendStr(edRow *row, char *s, size_t len);
@@ -112,6 +114,10 @@ void edRowAppendStr(edRow *row, char *s, size_t len);
 void edInsertChar(int c);
 void edRemoveChar();
 void edInsertNewline();
+
+// search
+void edSearch();
+void edSearchCallback(char *q, int k);
 
 /*** init ***/
 void init_editor() {
@@ -142,7 +148,7 @@ int main(int argc, char* argv[]) {
     edOpen(argv[1]);
   }
 
-  edSetSMessage("CTRL-S to save, CTRL-Q to quit.");
+  edSetSMessage("CTRL-S to save | CTRL-Q to quit | CTRL-F to search");
 
   while (1) {
     edRefreshScreen();
@@ -326,6 +332,10 @@ void edProcessStroke() {
       }
       edClearScreen();
       exit(0);
+      break;
+
+    case CTRL_KEY('f'):
+      edSearch();
       break;
 
     case CTRL_KEY('s'):
@@ -599,6 +609,19 @@ int edComputeRx(edRow *row, int cX) {
   return rX;
 }
 
+int edComputeCx(edRow *row, int rX) {
+  int rX_t = 0;
+  int cX;
+  for (cX = 0; cX < row->size; cX++) {
+    if (row->chars[cX] == '\t')
+      rX_t += (TAB_STOP - 1) - (rX_t % TAB_STOP);
+    rX_t++;
+
+    if (rX_t > rX) return cX;
+  }
+  return cX;
+}
+
 void edRowInsertChar(edRow *row, int at, int c) {
   if (at < 0 || at > row->size) at = row->size;
   row->chars = realloc(row->chars, row->size + 2);
@@ -684,7 +707,13 @@ void edDeleteRow(int at) {
 }
 
 void edSave() {
-  if (E.fname == NULL) return;
+  if (E.fname == NULL) {
+    E.fname = edPrompt("Save as (ESC to cancel): %s", NULL);
+    if (E.fname == NULL) {
+      edSetSMessage("Save aborted.");
+      return;
+    }
+  }
 
   int len;
   char *buf = edRowsToString(&len);
@@ -731,6 +760,111 @@ void edInsertNewline() {
   }
   E.cY++;
   E.cX = 0;
+}
+
+char *edPrompt(char *prompt, void (*callback)(char *, int)) {
+  size_t bSize = 128;
+  char *buf = malloc(bSize);
+
+  size_t bLen = 0;
+  buf[0] = '\0';
+
+  while (1) {
+    // similar control loop to main as we fill out the user's answer to the prompt
+    edSetSMessage(prompt, buf);
+    edRefreshScreen();
+
+    int c = edReadKey();
+    if (c == BACKSPACE || c == DEL_KEY || c == CTRL_KEY('h')) {
+      // delete current character
+      if (bLen != 0)
+        buf[--bLen] = '\0';
+    } else if (c == '\x1b') {
+      // esc. key to get out of answering prompt
+      edSetSMessage("");
+      free(buf);
+      if (callback) callback(buf, c); // cancel the search
+      return NULL;
+    } else if (c == '\r') {
+      // enter key to return user answer
+      if (bLen != 0) {
+        edSetSMessage("");
+        if (callback) callback(buf, c); // exit the search
+        return buf;
+      }
+    } else if (!iscntrl(c) && c < 128) {
+      // add to user's running answer
+      if (bLen == bSize - 1) {
+        bSize *= 2;
+        buf = realloc(buf, bSize);
+      }
+      buf[bLen++] = c;
+      buf[bLen] = '\0';
+    }
+
+    // for incremental search (we search as the query is built)
+    if (callback) callback(buf, c);
+  }
+}
+
+void edSearchCallback(char *q, int k) {
+  static int prevMatch = -1;
+  static int direction = 1; // 1 for forward, -1 for backward
+
+  // set up variables for moving through search results
+  if (k == '\r' || k == '\x1b') {
+    prevMatch = -1;
+    direction = 1;
+    return;
+  } else if (k == ARROW_RIGHT || k == ARROW_DOWN) {
+    direction = 1;
+  } else if (k == ARROW_LEFT || k == ARROW_UP) {
+    direction = -1;
+  } else {
+    prevMatch = -1;
+    direction = 1;
+  }
+
+  if (prevMatch == -1) direction = 1; // we search forward by default
+  int current = prevMatch;
+  int i;
+
+  // searches based on current, and current persists due to staticness.
+  // leads to ness-incremental.
+  for (i = 0; i < E.nRows; i++) {
+    current += direction;
+    if (current == -1) current = E.nRows - 1;
+    else if (current == E.nRows) current = 0;
+
+    edRow *row = &E.row[current];
+    char *match = strstr(row->render, q);
+    if (match) {
+      prevMatch = current;
+      E.cY = current;
+      E.cX = edComputeCx(row, match - row->render);
+      E.rowOff = E.nRows;
+      break;
+    }
+  }
+}
+
+void edSearch() {
+  int cX_t = E.cX;
+  int cY_t = E.cY;
+  int colOff_t = E.colOff;
+  int rowOff_t = E.rowOff;
+
+  char *q = edPrompt("Search token (ESC/ENTER/Arrows to navigate): %s", edSearchCallback);
+
+  if (q) {
+    free(q);
+  } else {
+    // we chose to not use the query => query is null, so we restore former position
+    E.cX = cX_t;
+    E.cY = cY_t;
+    E.colOff = colOff_t;
+    E.rowOff = rowOff_t;
+  }
 }
 
 int getWindowSize(int *rows, int *cols) {
